abbcfa378e5e95d025de6dcaf863b7b40107ac8e
Lukas Ehnle <lukas@owlabout.de>
2019-07-24 13:34:28 +0200

    feat: sticky container and styling

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -12,13 +12,25 @@
 
 <script>
   export let post;
+
+  $: sticky = post.html.indexOf("sticky__container") > 0;
 </script>
 
 <style>
   :global(.container) {
     padding: 0 2rem;
-    max-width: 102rem;
+    max-width: 136rem;
     margin: 0 auto;
+  }
+  :global(.sticky__container) {
+    max-width: 136rem;
+  }
+  .post {
+    display: flex;
+    flex-direction: column;
+    max-width: 68rem;
+    margin: 0 auto;
+    position: relative;
   }
 
   .post :global(h2) {
@@ -31,6 +43,25 @@
   .post :global(li) {
     margin: 0 0 0.5em 0;
   }
+  @media screen and (min-width: 1060px) {
+    .has-sticky {
+      margin-right: 0;
+    }
+    :global(.sticky__container) {
+      align-self: flex-end;
+      padding: 0 2rem;
+      margin: 0 -2rem;
+      width: 100vw;
+      display: grid;
+      grid-column-gap: 4rem;
+      grid-template-rows: auto;
+      grid-template-columns: minmax(0, 680px) 680px;
+    }
+    :global(.sticky) {
+      position: sticky;
+      top: 10rem;
+    }
+  }
 </style>
 
 <svelte:head>
@@ -38,7 +69,7 @@
 </svelte:head>
 
 <div class="container">
-  <div class="post">
+  <div class="post" class:has-sticky={sticky}>
     {@html post.html}
   </div>
 </div>

12a3140712e2ea660f20af97bbef4d9c6c51193f
becca <becca@owlabout.de>
2020-05-25 00:13:11 +0200

    add authors to front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,13 +38,13 @@
     background-color: #fff;
     background-image: url('/images/blog.svg');
     background-repeat: no-repeat;
-    background-position: 100% 30%;
+    background-position: 100% 25%;
     left: 0;
     top: 0;
   }
   .blog-info-container {
     position: absolute;
-    top: calc(30% + 14px);
+    top: calc(25% + 14px);
     left: 50%;
     width: 310px;
     transform: translateX(-50%);
@@ -54,6 +54,49 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+  .authors {
+    position: absolute;
+    bottom: 120px;
+    left: 50%;
+    width: 420px;
+    transform: translateX(-50%);
+  }
+  .author {
+    position: relative;
+    display: inline-block;
+  }
+  .author.lukas {
+    margin-right: 50px;
+  }
+  .author .author-desc {
+    background: url('/images/author-desc-frame.svg');
+    width: 175px;
+    height: 157px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 0.7em;
+    opacity: 0.7;
+  }
+  .author .author-desc .name {
+    font-weight: 900;
+    font-size: 1.6rem;
+    opacity: 1;
+    color: #3e3e3e;
+  }
+  .author .author-thumbnail {
+    position: absolute;
+  }
+  .author .author-thumbnail.lukas {
+    top: -60%;
+    right: -30px;
+  }
+  .author .author-thumbnail.becca {
+    bottom: -60%;
+    left: -30px;
   }
 </style>
 
@@ -73,6 +116,24 @@
     Symfony und Softwareentwicklung allgemein.</div>
   </div>
   <div class="authors">
+    <div class="author lukas">
+      <div class="author-desc">
+        <b class="name">Lukas Ehnle</b>
+        <div>Fullstack Developer</div>
+      </div>
+      <div class="author-thumbnail lukas">
+        <img src="/images/lukas-fp.png" alt="Lukas Ehnle">
+      </div>
+    </div>
+    <div class="author ">
+      <div class="author-desc">
+        <b class="name">Becca Roberts</b>
+        <div>Webdeveloper<br/>  Designer</div>
+      </div>
+      <div class="author-thumbnail becca">
+        <img src="/images/becca-fp.png" alt="Becca Roberts">
+      </div>
+    </div>
   </div>
 </div>
 <div class="excerpts-container">
